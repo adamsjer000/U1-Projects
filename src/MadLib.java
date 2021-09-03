39e3457c986d6535cb4907a14029be421bbf1d4a
adamsjer000 <adamsjer000@warren.k12.in.us>
2021-09-03 13:21:09 -0400

    Important

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,6 +1,23 @@
+import javax.swing.JOptionPane;
 public class MadLib {
+    public static void main(String[] args) {
 
     /*Create your own MadLibs. (https://www.madtakes.com/) You may use a story or example from the
     website if you don't wish to make up your own. This program must run entirely in the console.
      */
+
+
+        String place;
+        String verb;
+        String noun;
+
+        place = JOptionPane.showInputDialog("What's your favorite place to eat ?");
+        verb = JOptionPane.showInputDialog("What is something that describes you best ?");
+        noun = JOptionPane.showInputDialog("What should I call you ?");
+        JOptionPane.showMessageDialog(null, "Hey! + place + verb + noun");
+
+        System.exit(0);
+    }
+
+
 }
--- src/RPGCharacter.java
+++ src/RPGCharacter.java
@@ -1,5 +1,6 @@
+import javax.swing.*;
 public class RPGCharacter {
-
+    public static void main(String[] args) {
     /*Create a program that calculates an RPG Character's battle stats based on the character's physical stats.
     The physical and battle stat can be anything you wish provided it meets these criteria:
 
@@ -9,5 +10,17 @@ public class RPGCharacter {
      We will continue to expand on this program throughout the year, so give it some thought. There should be some complexity.
 
      The program should run using dialog boxes and the output should show ALL the character's stats including a name.
-     */
+     * */
+        
+        String characterStrength = JOptionPane.showInputDialog("what is your character strength ?") ;
+        String characterDexterity = JOptionPane.showInputDialog("what is your character dexterity ?");
+        String characterSpirit = JOptionPane.showInputDialog("what is your character spirit ?");
+        String characterDiversity = JOptionPane.showInputDialog("what is your character diversity ?");
+        String characterFlexibility = JOptionPane.showInputDialog("what is your character flexibility ?");
+        String characterJump = JOptionPane.showInputDialog("what is your characters jumping ability ?");
+
+
+
+
+    }
 }
